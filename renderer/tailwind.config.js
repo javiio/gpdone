bb90408dd05ecd1f085ed2e38c008418ea7cfee1
Javier Orellana <javier.orellana.vb@gmail.com>
2023-05-08 15:39:13 -0400

    Config tailwind &  nextjs

--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
     './renderer/pages/**/*.{js,ts,jsx,tsx}',
-    './renderer/components/**/*.{js,ts,jsx,tsx}',
+    './renderer/libs/**/components/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors: {
@@ -11,6 +11,8 @@ module.exports = {
       white: colors.white,
       gray: colors.gray,
       blue: colors.blue,
+      slate: colors.slate,
+      yellow: colors.yellow,
     },
     extend: {},
   },
