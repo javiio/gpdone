1c2097ea863e794794f24831149ea547d02811ba
Javier Orellana <javier.orellana.vb@gmail.com>
2023-05-08 15:38:25 -0400

    Add firebase support

---
+++ renderer/libs/platform/data/index.ts
@@ -0,0 +1,63 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc as setFirebaseDoc,
+  updateDoc as updateFirebaseDoc,
+  arrayUnion,
+} from 'firebase/firestore';
+import {
+  useCollection as useFirebaseCollection,
+  useDocument as useFirebaseDoc,
+} from 'react-firebase-hooks/firestore';
+import firebaseConfig from './firebase.config';
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const currentUser = {
+  id: 'javi',
+};
+
+export const useCollection = (path: string) =>
+  useFirebaseCollection(
+    collection(getFirestore(firebaseApp), `users/${currentUser.id}/${path}`),
+    { snapshotListenOptions: { includeMetadataChanges: true } }
+  );
+
+export const useDoc = (...path: string[]) => {
+  return useFirebaseDoc(
+    doc(getFirestore(firebaseApp), `users/${currentUser.id}`, ...path),
+    { snapshotListenOptions: { includeMetadataChanges: true } }
+  );
+};
+
+export const setDoc = async (data: object, ...path: string[]) => {
+  await setFirebaseDoc(
+    doc(getFirestore(firebaseApp), `users/${currentUser.id}`, ...path),
+    data,
+    { merge: true }
+  );
+};
+
+export const updateDoc = async (data: object, ...path: string[]) => {
+  const docRef = doc(
+    getFirestore(firebaseApp),
+    `users/${currentUser.id}`,
+    ...path
+  );
+  await updateFirebaseDoc(docRef, data);
+};
+
+export const addItemToArrayDoc = async (
+  data: object,
+  attribute: string,
+  ...path: string[]
+) => {
+  const docRef = doc(
+    getFirestore(firebaseApp),
+    `users/${currentUser.id}`,
+    ...path
+  );
+  await updateFirebaseDoc(docRef, { [attribute]: arrayUnion(data) });
+};
