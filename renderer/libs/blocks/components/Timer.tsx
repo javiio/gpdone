84d915a904cf2f19a312a29284b3a3d723f5123a
Javier Orellana <javier.orellana.vb@gmail.com>
2023-05-19 16:35:20 -0400

    Match timer color with current project color

--- renderer/libs/blocks/components/Timer.tsx
+++ renderer/libs/blocks/components/Timer.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
+import cn from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRefresh } from '@fortawesome/free-solid-svg-icons';
-import { useTimer } from '../';
+import { useTimer, useCurrentBlock } from '../';
 
 const Timer: React.FC = () => {
   const {
@@ -12,6 +13,7 @@ const Timer: React.FC = () => {
     isPaused,
     formatTime,
   } = useTimer();
+  const { color } = useCurrentBlock();
 
   const handleKeyDown = (event: KeyboardEvent) => {
     if (event.key === 's' && event.metaKey) {
@@ -27,13 +29,16 @@ const Timer: React.FC = () => {
   }, []);
 
   return (
-    <div className="opacity-70">
+    <div>
       {isPaused && remainingTime !== blockTime && (
         <button onClick={resetTimer}>
-          <FontAwesomeIcon icon={faRefresh} className="mr-1.5" />
+          <FontAwesomeIcon icon={faRefresh} className="mr-1.5 opacity-70" />
         </button>
       )}
-      <button onClick={toggleTimer} className="text-2xl">
+      <button
+        onClick={toggleTimer}
+        className={cn('text-4xl', isPaused ? 'opacity-70' : `text-${color}`)}
+      >
         {formatTime()}
       </button>
     </div>
--- renderer/libs/platform/components/DynamicStylesGenerator.tsx
+++ renderer/libs/platform/components/DynamicStylesGenerator.tsx
@@ -6,12 +6,12 @@ export const DynamicStylesGenerator = () => {
     <div
       className={cn(
         'hidden',
-        'border-gray-400 bg-gray-400 bg-gray-400/10 bg-gray-400/50 hover:bg-gray-400/25 focus:outline-gray-400',
-        'border-yellow-500 bg-yellow-500 bg-yellow-500/10 bg-yellow-500/50 hover:bg-yellow-500/25 focus:outline-yellow-500',
-        'border-green-500 bg-green-500 bg-green-500/10 bg-green-500/50 hover:bg-green-500/25 focus:outline-green-500',
-        'border-sky-500 bg-sky-500 bg-sky-500/10 bg-sky-500/50 hover:bg-sky-500/25 focus:outline-sky-500',
-        'border-orange-500 bg-orange-500 bg-orange-500/10 bg-orange-500/50 hover:bg-orange-500/25 focus:outline-orange-500',
-        'border-purple-500 bg-purple-500 bg-purple-500/10 bg-purple-500/50 hover:bg-purple-500/25 focus:outline-purple-500'
+        'border-gray-400 bg-gray-400 bg-gray-400/10 bg-gray-400/50 hover:bg-gray-400/25 focus:outline-gray-400 text-gray-400',
+        'border-yellow-500 bg-yellow-500 bg-yellow-500/10 bg-yellow-500/50 hover:bg-yellow-500/25 focus:outline-yellow-500 text-yellow-500',
+        'border-green-500 bg-green-500 bg-green-500/10 bg-green-500/50 hover:bg-green-500/25 focus:outline-green-500 text-green-500',
+        'border-sky-500 bg-sky-500 bg-sky-500/10 bg-sky-500/50 hover:bg-sky-500/25 focus:outline-sky-500 text-sky-500',
+        'border-orange-500 bg-orange-500 bg-orange-500/10 bg-orange-500/50 hover:bg-orange-500/25 focus:outline-orange-500 text-orange-500',
+        'border-purple-500 bg-purple-500 bg-purple-500/10 bg-purple-500/50 hover:bg-purple-500/25 focus:outline-purple-500 text-purple-500'
       )}
     />
   );
