1b571f7e1fba4c26c6d4b76229b6cf4068251eed
Javier Orellana <javier.orellana.vb@gmail.com>
2023-05-24 16:45:50 -0400

    Highlight planned blocks that has been achieved

--- renderer/libs/planning/components/PlanningList.tsx
+++ renderer/libs/planning/components/PlanningList.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cn from 'classnames';
 import type { DateTime } from 'luxon';
-import { usePlanning } from '../';
+import { usePlanning } from '~planning';
+import { useDailyBlocks, type Block } from '~blocks';
 
 export const PlanningList = ({ date }: { date?: DateTime }) => {
   const { planning } = usePlanning(date);
+  const { blocks } = useDailyBlocks(date);
+  const [highlightedPlanning, setHighlightedPlanning] = useState<boolean[]>([]);
+
+  useEffect(() => {
+    if (planning && blocks) {
+      const newHighlightedPlanning: boolean[] = [];
+      const projectBlockCount = new Map<string, number>();
+
+      // Count number of blocks per project
+      blocks.forEach((block: Block) => {
+        projectBlockCount.set(block.projectId, (projectBlockCount.get(block.projectId) ?? 0) + 1);
+      });
+
+      // Set project plan to be highlighted if there are blocks for it
+      planning.forEach((project) => {
+        const count = projectBlockCount.get(project?.id) ?? 0;
+        newHighlightedPlanning.push(count > 0);
+        if (count > 0) {
+          projectBlockCount.set(project?.id, count - 1);
+        }
+      });
+
+      setHighlightedPlanning(newHighlightedPlanning);
+    }
+  }, [planning, blocks]);
 
   return (
     <ul>
@@ -12,8 +38,9 @@ export const PlanningList = ({ date }: { date?: DateTime }) => {
         <li
           key={i}
           className={cn(
-            'border rounded-md overflow-hidden w-32 my-2 py-3 text-center opacity-80',
-            `border-${project?.color}/75 bg-${project?.color}/10`
+            'border rounded-md overflow-hidden w-32 my-2 py-3 text-center',
+            highlightedPlanning[i] ? `border-${project?.color}/50 bg-${project?.color}/50` : `border-${project?.color}/75 bg-${project?.color}/10`,
+            highlightedPlanning[i] ? '' : 'opacity-80'
           )}
         >
           {project?.name}
