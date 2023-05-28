ec36bae529ecca748d5c2898cbb5dcd696da4c49
Javier Orellana <javier.orellana.vb@gmail.com>
2023-05-27 22:30:04 -0400

    Load notes only once from firebase

--- renderer/libs/notes/hooks/useNotes.ts
+++ renderer/libs/notes/hooks/useNotes.ts
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import { EditorState, convertToRaw, convertFromRaw } from 'draft-js';
 import { debounce } from 'lodash';
-import { useDoc, updateDoc } from '~platform';
+import { useDocOnce, updateDoc } from '~platform';
 import type { Note } from '../';
 
 export const useNote = (noteId: string) => {
   const [note, setNote] = useState<Note>();
-  const [data, isLoading, error] = useDoc('notes', noteId);
+  const [data, isLoading, error] = useDocOnce('notes', noteId);
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
   useEffect(() => {
