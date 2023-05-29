8858d326857392d6929b46bb21e515b302a9e4e2
Javier Orellana <javier.orellana.vb@gmail.com>
2023-05-28 23:36:41 -0400

    Add planning page

---
+++ renderer/pages/planning.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Head from 'next/head';
+import { useRecoilValue } from 'recoil';
+import { currentDateState } from '~blocks';
+import { PlanningList, PlanningForm } from '~planning';
+import { NoteEditor } from '~notes';
+
+const Home = () => {
+  const currentDate = useRecoilValue(currentDateState);
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Planning</title>
+      </Head>
+      <div className="flex">
+        <div className="w-lg relative flex-1">
+          <PlanningForm date={currentDate} />
+          <PlanningList date={currentDate} />
+        </div>
+
+        <div className="bg-slate-700 flex-1 h-screen">
+          <NoteEditor noteId="planning" />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Home;
