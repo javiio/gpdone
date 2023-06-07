7d8ca526b9a8ad122000325510ae55b04224cf34
Javier Orellana <javier.orellana.vb@gmail.com>
2023-06-07 14:27:05 -0400

    Improve Projects selection flow

--- renderer/libs/projects/components/ProjectSelector.tsx
+++ renderer/libs/projects/components/ProjectSelector.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import cn from 'classnames';
 import { useCurrentBlock } from '~blocks';
-import { useProjects } from '../';
+import { type Project, useProjects } from '../';
 
 export const ProjectSelector = () => {
   const { currentBlock, updateProject } = useCurrentBlock();
@@ -9,23 +9,22 @@ export const ProjectSelector = () => {
   const [showAll, setShowAll] = useState(false);
 
   const handleKeyDown = (event: KeyboardEvent) => {
-    if (event.metaKey) {
-      if (event.key === 'p') {
-        event.preventDefault();
-        setShowAll(true);
-      } else if (showAll) {
-        projects?.forEach((project, i) => {
-          const key = `${(i + 1)}`;
-          if (event.key === key) {
-            event.preventDefault();
-            updateProject(project);
-            setShowAll(false);
-          }
-        });
-        if (event.key === 'Escape') {
+    if (event.metaKey && event.key === 'p') {
+      event.preventDefault();
+      setShowAll((prev) => !prev);
+    }
+    if (showAll) {
+      projects?.forEach((project, i) => {
+        const key = `${(i + 1)}`;
+        if (event.key === key) {
           event.preventDefault();
+          updateProject(project);
           setShowAll(false);
         }
+      });
+      if (event.key === 'Escape') {
+        event.preventDefault();
+        setShowAll(false);
       }
     }
   };
@@ -37,6 +36,15 @@ export const ProjectSelector = () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, [projects, showAll]);
+
+  const handleSelectedProject = (project: Project) => {
+    if (showAll) {
+      updateProject(project);
+      setShowAll(false);
+    } else {
+      setShowAll(true);
+    }
+  };
 
   return (
     <ul className="text-xs flex space-x-2">
@@ -51,20 +59,17 @@ export const ProjectSelector = () => {
               `border-${project.color} bg-${project.color}/10`
             )}
           >
-            {project.id === currentBlock?.project?.id
-              ? (<div className={`px-1.5 py-0.5 bg-${project.color}/50`}>
-                  {project.name}
-                </div>
-                )
-              : (<button
-                  type="button"
-                  onClick={() => { updateProject(project); }}
-                  className={`px-1.5 py-0.5 hover:bg-${project.color}/25`}
-                >
-                  {project.name}
-                </button>
-                )
-            }
+            <button
+              type="button"
+              onClick={() => { handleSelectedProject(project); }}
+              className={cn(
+                'px-1.5 py-0.5',
+                `hover:bg-${project.color}/25`,
+                project.id === currentBlock?.project?.id && `bg-${project.color}/50`
+              )}
+            >
+              {project.name}
+            </button>
           </li>
         ))}
     </ul>
