88b34ea35ce8af84431bce84049141ec9796a795
Javier Orellana <javier.orellana.vb@gmail.com>
2023-06-13 13:36:28 -0400

    Show a gray progresbar when paused

--- renderer/libs/timer/components/TimerProgressLine.tsx
+++ renderer/libs/timer/components/TimerProgressLine.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import cn from 'classnames';
 import { useCurrentBlock } from '~blocks';
 import { useTimer } from '../';
 
 export const TimerProgressLine = () => {
-  const { progress } = useTimer();
+  const { progress, isPaused } = useTimer();
   const { color } = useCurrentBlock();
 
   return (
     <div className="h-2">
       <div
-        className={`bg-${color} h-full`}
+        className={cn('h-full', isPaused ? 'bg-gray-500' : `bg-${color}`)}
         style={{ width: `${(100 - progress).toString()}%` }}
       />
     </div>
