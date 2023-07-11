af6209d8b81de8d4906336e959ee879121a8ddf9
Javier Orellana <javier.orellana.vb@gmail.com>
2023-07-11 08:19:55 -0400

    Remove old project selector

--- renderer/libs/projects/index.ts
+++ renderer/libs/projects/index.ts
@@ -1,4 +1,3 @@
-export * from './components/ProjectSelector';
 export * from './components/ProjectCombobox';
 export * from './components/ProjectsPillTabs';
 export * from './components/ProjectsTabs';
