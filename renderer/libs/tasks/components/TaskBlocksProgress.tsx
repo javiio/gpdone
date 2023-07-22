5ce5d860e5f552ab7cce867c6e0d31a086bf28ab
Javier Orellana <javier.orellana.vb@gmail.com>
2023-07-22 01:57:37 -0400

    Fix endmark on task progress view

--- renderer/libs/tasks/components/TaskBlocksProgress.tsx
+++ renderer/libs/tasks/components/TaskBlocksProgress.tsx
@@ -9,11 +9,11 @@ export const TaskBlocksProgress = ({ task }: { task: Task }) => {
   return (
     <div className="flex space-x-[3px] items-center">
       {task.blocksIds?.map((_, i) => (
-        <div key={i++}>
-          <div className={`w-2.5 h-2.5 rounded-full bg-${color}`} />
+        <div key={i++} className="flex items-center">
           {i === task.plannedBlocks && (
-            <div className="w-[1px] h-5 bg-red-500" key={`${i}-divider`} />
+            <div className="w-[1px] h-5 bg-red-500 mr-1" />
           )}
+          <div className={`w-2.5 h-2.5 rounded-full bg-${color}`} />
         </div>
       ))}
       {pending > 0 && Array(pending).fill(0).map(() => (
