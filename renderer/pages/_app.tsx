8386c214232864ffce54f979313265a72d47ec17
Javier Orellana <javier.orellana.vb@gmail.com>
2023-08-25 09:45:35 -0400

    Fix: add '.html' to the route in prod to avoid empty page on reload

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { DynamicStylesGenerator, ProvideConfirmation } from '~platform';
 import { ProvideCurrentBlock, CurrentBlock } from '~blocks';
 import { ProvideTimer } from '~timer';
@@ -10,7 +11,22 @@ import { Navbar } from 'layout/Navbar';
 import '../styles/globals.css';
 import '../styles/editor.css';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const GPDApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Fix for empty page on production
+    // https://github.com/saltyshiomix/nextron/issues/241#issuecomment-1239401532
+    if (isProd) {
+      if (window.location.href.search(/\d/) > 0) return;
+      if (window.location.href.search('.html') < 0) {
+        window.location.replace(`${window.location.href}.html`);
+      }
+    }
+  }, [router.asPath]);
+
   return (
     <ProvideConfirmation>
       <ProvideProjects>
