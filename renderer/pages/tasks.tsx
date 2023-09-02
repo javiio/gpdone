42c80addd626f7c2c3820a3ada8cc289b0d2af52
Javier Orellana <javier.orellana.vb@gmail.com>
2023-09-02 10:21:40 -0400

    Save in cookies the current project for tasks

--- renderer/pages/tasks.tsx
+++ renderer/pages/tasks.tsx
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import { useCookie } from 'react-use';
 import { ResizablePanels } from '~platform';
-import { ProjectsTabs, type Project } from '~projects';
+import { ProjectsTabs, type Project, useProjects } from '~projects';
 import { TaskForm, TasksList, TaskInfo, useTasks } from '~tasks';
 
 const Tasks = () => {
   const [project, setProject] = useState<Project | undefined>(undefined);
+  const [projectId, setProjectId] = useCookie('tasks-project-id');
   const { selectedTask } = useTasks();
+  const { getProject } = useProjects();
+
+  const handleSetProject = (project: Project) => {
+    setProject(project);
+    setProjectId(project?.id);
+  };
+
+  useEffect(() => {
+    if (projectId && !project) {
+      setProject(getProject(projectId));
+    }
+  }, [projectId, getProject]);
 
   return (
     <React.Fragment>
@@ -15,7 +29,7 @@ const Tasks = () => {
       </Head>
       <ResizablePanels vertical aSize="40%" bSize="60%">
         <div className="p-4 pt-2">
-          <ProjectsTabs project={project} setProject={setProject} />
+          <ProjectsTabs project={project} setProject={handleSetProject} />
           <div className="my-4">
             <TaskForm project={project} />
           </div>
